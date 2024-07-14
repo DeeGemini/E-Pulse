743841e85242bd404948e2d187a782c458887170
DeeGemini <zodiactheegemini@gmail.com>
2024-07-14 16:32:50 +0200

    This is where all the wallpaper are

--- E-Pulse/screens/WallpaperScreen.js
+++ E-Pulse/screens/WallpaperScreen.js
@@ -5,19 +5,27 @@ import * as MediaLibrary from 'expo-media-library';
 
 export default function WallpaperScreen() {
 	const setWallpaper = async () => {
-		const {status} = await MediaLibrary.requestPermissionsAsync();
-		if (status === 'granted') {
-			// Code to set wallpaper goes here
-			alert('Wallpaper set successfully');
-		}else {
-			alert('Permission denied');
+		try {
+			const {granted} = await MediaLibrary.requestPermissionsAsync();
+			if (granted) {
+				// Code to set wallpaper goes here
+				alert('Wallpaper set successfully');
+			} else {
+				alert('Permission denied');
+			}
+		} catch (error) {
+			if (error instanceof Error) {
+				console.error('Error while setting wallpaper:', error.message);
+			} else {
+				console.error('Unknown error while setting wallpaper');
+			}
 		}
 	};
 
 	return (
 		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-		 <Text>Select your fav Wallpaper</Text>
-		 <Button title="Set as Wallpaper" onPress={setWallpaper} />
+			<Text>Select your favorite Wallpaper</Text>
+			<Button title="Set as Wallpaper" onPress={setWallpaper} />
 		</View>
 	);
 }
