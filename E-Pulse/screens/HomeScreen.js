bb040e1bb398f7321cd2e335361c647f87f8446a
DeeGemini <zodiactheegemini@gmail.com>
2024-07-14 16:30:45 +0200

    First screen the user will see

--- E-Pulse/screens/HomeScreen.js
+++ E-Pulse/screens/HomeScreen.js
@@ -2,14 +2,23 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 
-export default function
-HomeScreen({navigation}) {
-	return(
-		<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-		 <Text>Welcome to E-Pulse</Text>
-		 <Button title="Open Calendar" onPress={() => navigation.navigate('Calendar')} />
-		 <Button title="Set Wallpaper" onPress={() => navigation.navigate('Wallpaper')} />
-		</view>
-	);
+export default function HomeScreen({navigation}) {
+  if (!navigation) {
+    throw new Error('Navigation prop is missing');
+  }
+
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Welcome to E-Pulse</Text>
+      <Button
+        title="Open Calendar"
+        onPress={() => navigation.navigate('Calendar')}
+      />
+      <Button
+        title="Set Wallpaper"
+        onPress={() => navigation.navigate('Wallpaper')}
+      />
+    </View>
+  );
 }
 
