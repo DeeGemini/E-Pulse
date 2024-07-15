73b86696ddd16e4c9a141b2c61d375e96630eb4e
DeeGemini <zodiactheegemini@gmail.com>
2024-07-15 14:14:24 +0200

    Exported the main navigation components

--- E-Pulse/navigation/MainNavigator.js
+++ E-Pulse/navigation/MainNavigator.js
@@ -2,23 +2,58 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createAppContainer} from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
 import CalendarScreen from '../screen/CalendarScreen';
 import WallpaperScreen from '../screen/WallpaperScreen';
 
-const Stack = createStackNavigator();
+const MainNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Calendar: CalendarScreen,
+    Wallpaper: WallpaperScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default function MainNavigator() {
+  if (!NavigationContainer) {
+    throw new Error('NavigationContainer component not found');
+  }
+
+  if (!Stack.Navigator) {
+    throw new Error('Stack.Navigator component not found');
+  }
+
+  if (!screenNames) {
+    throw new Error('screenNames variable not found');
+  }
+
+  if (!screenComponents) {
+    throw new Error('screenComponents variable not found');
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        {Object.keys(screenNames).map((name) => (
-          <Stack.Screen
-            key={name}
-            name={screenNames[name]}
-            component={screenComponents[name]}
-          />
-        ))}
+        {Object.keys(screenNames).map((name) => {
+          const screenName = screenNames[name];
+          const screenComponent = screenComponents[name];
+
+          if (!screenName || !screenComponent) {
+            throw new Error(`Invalid screen name or component: ${name}`);
+          }
+
+          return (
+            <Stack.Screen
+              key={name}
+              name={screenName}
+              component={screenComponent}
+            />
+          );
+        })}
       </Stack.Navigator>
     </NavigationContainer>
   );
