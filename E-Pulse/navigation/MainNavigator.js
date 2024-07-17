41566e3fedf1816cefed0d7afe4e418bc72a0111
DeeGemini <zodiactheegemini@gmail.com>
2024-07-17 20:08:45 +0200

    Removed Duplicate Declaration

--- E-Pulse/navigation/MainNavigator.js
+++ E-Pulse/navigation/MainNavigator.js
@@ -6,67 +6,18 @@ import HomeScreen from '../screens/HomeScreen';
 import CalendarScreen from '../screen/CalendarScreen';
 import WallpaperScreen from '../screen/WallpaperScreen';
 
-const MainNavigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Calendar: CalendarScreen,
-    Wallpaper: WallpaperScreen,
-  },
-  {
-    initialRouteName: 'Home',
-  }
-);
+const Stack = createStackNavigator();
 
-export default function MainNavigator() {
-  if (!NavigationContainer) {
-    throw new Error('NavigationContainer component not found');
-  }
-
-  if (!Stack.Navigator) {
-    throw new Error('Stack.Navigator component not found');
-  }
-
-  if (!screenNames) {
-    throw new Error('screenNames variable not found');
-  }
-
-  if (!screenComponents) {
-    throw new Error('screenComponents variable not found');
-  }
-
+const MainNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        {Object.keys(screenNames).map((name) => {
-          const screenName = screenNames[name];
-          const screenComponent = screenComponents[name];
-
-          if (!screenName || !screenComponent) {
-            throw new Error(`Invalid screen name or component: ${name}`);
-          }
-
-          return (
-            <Stack.Screen
-              key={name}
-              name={screenName}
-              component={screenComponent}
-            />
-          );
-        })}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Calendar" component={CalendarScreen} />
+        <Stack.Screen name="Wallpaper" component={WallpaperScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
-
-const screenNames = {
-  Home: 'Home',
-  Calendar: 'Calendar',
-  Wallpaper: 'Wallpaper',
 };
 
-const screenComponents = {
-  Home: HomeScreen,
-  Calendar: CalendarScreen,
-  Wallpaper: WallpaperScreen,
-};
-
+export default MainNavigator;
